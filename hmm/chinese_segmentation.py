e73a9d2649fde4560a6aa49645b9a1855414ed44
yangwjd <yangwjd@chanjet.com>
2016-12-11 15:16:09 +0800

    中文分词

--- hmm/chinese_segmentation.py
+++ hmm/chinese_segmentation.py
@@ -1,47 +1,146 @@
-#-*-coding:utf-8-*-
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+import math
+import numpy as np
+
 
 class ChineseSegmentation:
 
-    _B = 0
-    _M = 1
-    _E = 2
-    _S = 3
+    _STATUS = (_B, _M, _E, _S) = range(4)
+    _DEFAULT_LOG_PROB = -3.14e+100
+    _STATUS_MAP = {_B: 'B', _M: 'M', _E: 'E', _S: 'S'}
 
     def __init__(self):
-        self.init_map = {}
-        self.b_map = {}
-        self.m_map = {}
-        self.e_map = {}
-        self.s_map = {}
-        self.status_map = {}
-
-    # 非空且全为汉字
-    @staticmethod
-    def all_chinese(text):
-        return text and all(u'\u4e00' <= char <= u'\u9fff' for char in text)
+        self.emit_map = map(lambda x: {}, self._STATUS)
+        self.trans_matrix = map(lambda x: map(lambda y: 0, self._STATUS), self._STATUS)
+        self.init_status = map(lambda x: 0, self._STATUS)
 
     @staticmethod
-    def inc_value(m, key):
+    def _inc_value(m, key):
         cnt = m.setdefault(key, 0)
         m[key] = cnt + 1
 
-    @staticmethod
-    def judge_staus():
+    def add_train_line(self, line):
+        items = filter(lambda x: len(x) > 0, line.split(" "))
+        if len(items) == 0:
+            return
 
-    def process_line(self, line):
-        items = filter(self.all_chinese, line.split(" "))
-        is_first = True
-        last_status = ""
+        status = []
+
+        # 转换成状态
         for item in items:
-            # 处理初始状态
-            if is_first:
-                is_first = False
-                if (len(item == 1)):
-                    self.inc_value(self.init_map, "S")
-                else:
-                    self.inc_value(self.init_map, "B")
+            if len(item) == 1:
+                status.append(self._S)
             else:
-                pass
+                status.append(self._B)
+                for i in range(1, len(item) - 1):
+                    status.append(self._M)
+                status.append(self._E)
+
+        pairs = zip("".join(items), status)
+        self.init_status[pairs[0][1]] += 1
+        self._inc_value(self.emit_map[pairs[0][1]], pairs[0][0])
+
+        for i in range(len(pairs)):
+            this_pair = pairs[i]
+            last_pair = pairs[i-1]
+            self.trans_matrix[last_pair[1]][this_pair[1]] += 1
+            self._inc_value(self.emit_map[this_pair[1]], this_pair[0])
+
+    def _cal_prob(self):
+        # 计算emit概率的log值
+        print "trans matrix"
+        for trans in self.trans_matrix:
+            print ','.join(map(lambda x: str(x), trans))
+        print "init status"
+        print ",".join(map(lambda x:str(x), self.init_status))
+
+        for emit in self.emit_map:
+            total_cnt = sum(emit.values())
+            for key in emit.keys():
+                emit[key] = self._DEFAULT_LOG_PROB if emit[key] == 0 \
+                    else math.log(emit[key] / total_cnt)
+
+        # 计算转移概率的log值
+        for trans in self.trans_matrix:
+            total = sum(trans)
+            for i in range(len(trans)):
+                trans[i] = self._DEFAULT_LOG_PROB if trans[i] == 0 \
+                    else math.log(trans[i] / total)
+
+        # 计算初始状态概率的log值
+        total = sum(self.init_status)
+        for i in range(len(self.init_status)):
+            self.init_status[i] = self._DEFAULT_LOG_PROB if self.init_status[i] == 0 \
+                else math.log(self.init_status[i] / total)
+
+    def train(self, lines=[]):
+        for line in lines:
+            self.add_train_line(line)
+
+        self._cal_prob()
+
+    def segment(self, line):
+        if not line:
+            return []
+        path = []
+        path.append(map(lambda x: -1, self._STATUS))
+        last_max = []
+        for i in range(len(self._STATUS)):
+            last_max.append(self.init_status[i] +
+                            self.emit_map[i].get(line[0], self._DEFAULT_LOG_PROB))
+
+        for i in range(1, len(line)):
+            this_path = map(lambda x: -1, self._STATUS)
+            this_max = map(lambda x: self._DEFAULT_LOG_PROB, self._STATUS)
+
+            for m in range(len(self._STATUS)):
+                emit_prob = self.emit_map[m].get(line[i], self._DEFAULT_LOG_PROB)
+                probs = [last_max[n] + self.trans_matrix[n][m] + emit_prob for n in range(len(self._STATUS))]
+                this_path[m] = np.argmax(probs)
+                this_max[m] = np.max(probs)
+
+            path.append(this_path)
+            last_max = this_max
+
+        last_index = np.argmax(last_max)
+        status = []
+        for i in range(len(line) - 1, -1, -1):
+            status.append(self._STATUS_MAP[last_index])
+            last_index = path[i][last_index]
+
+        status.reverse()
+        return status
+
+
+if __name__ == '__main__':
+    train_path = "icwb2-data/training/pku_training.utf8"
+    model = ChineseSegmentation()
+    # with open(train_path) as target:
+    #     for line in target.readline():
+    #         pass
+    #         # model.add_train_line(unicode(line))
+    lines = open(train_path).readlines()
+    for line in lines:
+        model.add_train_line(line.decode('utf8').strip())
+
+    model.train()
+
+    while(True):
+        text = raw_input("")
+        text = text.decode('utf8').strip()
+        status = model.segment(text)
+        result = ""
+        for (c, s) in zip(text, status):
+            result += c
+            if s == 'E' or s == 'S':
+                result += '\\'
+
+        print result
+
+
 
 
 
